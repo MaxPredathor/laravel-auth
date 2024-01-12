56b27a016fc8232167a62da0d8fc2a98291c5f7b
MaxPredathor <max.gilli98@gmail.com>
2024-01-12 14:11:04 +0100

    added html decode

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="container">
         <h1>{{ $project->title }}</h1>
-        <p>{{ $project->body }}</p>
+        <p>{!! $project->body !!}</p>
         <button class="btn btn-primary d-inline-block"><a class="text-white text-decoration-none"
                 href="{{ route('admin.projects.edit', $project->id) }}">Edit</button>
         <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
