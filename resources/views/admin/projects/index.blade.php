6755f2bcf3e2a200110e1362b9729f2eb5aab6c6
MaxPredathor <max.gilli98@gmail.com>
2024-01-12 13:53:02 +0100

    d-block

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,7 @@
     <section class="container">
         <h1>Projects List</h1>
         @foreach ($projects as $project)
-            <a class="mt-2" href="{{ route('admin.projects.show', $project->id) }}">{{ $project->title }}</a> <br>
+            <a class="mt-2 d-block" href="{{ route('admin.projects.show', $project->id) }}">{{ $project->title }}</a>
         @endforeach
         <button class="btn btn-primary mt-3">
             <a class="text-white text-decoration-none" href="{{ route('admin.projects.create') }}">Create</a>
